f9e10b79aaa3b073123057511ee7fb6eb51cb4f4
echoline <echoline@gmail.com>
2021-03-27 17:52:31 -0700

    fetch script

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 cd src/main &&
-hg clone https://hg.libsdl.org/SDL/ &&
+test -d SDL || hg clone https://hg.libsdl.org/SDL/ &&
 for r in ctf rogue xatrix yquake2; do
-	git clone https://github.com/yquake2/${r}
+	test -d ${r} || git clone https://github.com/yquake2/${r}
 done &&
-patch -p1 < yquake2.patch &&
-cd ../../.. &&
+cd yquake2 &&
+patch -p1 < ../yquake2.patch &&
+cd ../../../.. &&
 patch -p1 < Quake2VR/cardboard.patch &&
 cd Quake2VR &&
 echo YAYYYYYYYYY
